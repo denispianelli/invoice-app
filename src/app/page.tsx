a1713d0b30f2c2e9459c952b61fdbb56e5f7d686
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-21 10:35:05 +0200

    chore: Comment out unnecessary code in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  const session = await auth();
-  if (!session) {
-    redirect('/login');
-  } else {
-    redirect('/invoices');
-  }
+  // const session = await auth();
+  // if (!session) {
+  //   redirect('/login');
+  // } else {
+  //   redirect('/invoices');
+  // }
+
+  return <div>Home</div>;
 }
