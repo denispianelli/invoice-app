cf7a1ef5db986b2c3541055765bbb4b1f2d380f1
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-21 10:47:27 +0200

    chore: Remove unnecessary code in Home component and update redirect logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  // const session = await auth();
-  // if (!session) {
-  //   redirect('/login');
-  // } else {
-  //   redirect('/invoices');
-  // }
-
-  return <div>Home</div>;
+  redirect('/invoices');
 }
