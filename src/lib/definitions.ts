394e01be8916309d0fae72dcbf8ff93160b1517e
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-21 10:06:42 +0200

    feat: Add Invoice type definition

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -10,3 +10,18 @@ export type User = {
   otp_expires: Date | null;
   image: string | null;
 };
+
+export type Invoice = {
+  id: string;
+  created_at: Date;
+  payment_due: Date;
+  description: string;
+  payment_terms: number;
+  client_name: string;
+  client_email: string;
+  status: 'paid' | 'pending' | 'draft';
+  total: number;
+  sender_address_id: string;
+  client_address_id: string;
+  user_id: string;
+};
