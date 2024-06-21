c37696f23bf6f38e5145a20002fc8e07d3126ba8
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-21 11:18:03 +0200

    chore: Update Home component to redirect to invoices page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  return (
-    <div>
-      <h1>Home</h1>\{' '}
-    </div>
-  );
+  redirect('/invoices');
 }
