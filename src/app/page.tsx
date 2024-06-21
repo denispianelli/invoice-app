e19f105060cfe8c8230a85e7c02be35e1a88b5f4
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-21 11:00:06 +0200

    chore: Update Home component to return a basic HTML structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  redirect('/invoices');
+  return (
+    <div>
+      <h1>Home</h1>\{' '}
+    </div>
+  );
 }
