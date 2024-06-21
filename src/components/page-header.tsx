8728a1c45445d018d299b716e0e4238d4453d398
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-21 10:06:23 +0200

    chore: Update page header with link to home page

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -3,13 +3,17 @@ import { ModeToggleButton } from './mode-toggle-button';
 
 import { UserMenu } from './user-menu';
 import UserAuth from './user-auth';
+import Link from 'next/link';
 
 export async function Header() {
   const session = await auth();
 
   return (
-    <header className="fixed top-0 z-50 flex h-[72px] w-full justify-between bg-[#373B53] md:h-[80px] lg:h-screen lg:w-[103px] lg:flex-col lg:items-center lg:rounded-r-2xl">
-      <div className="bg-first relative grid w-[72px] place-content-center overflow-hidden rounded-r-2xl lg:h-[103px] lg:w-full">
+    <header className="sticky top-0 z-50 flex h-[72px] w-full justify-between bg-[#373B53] md:h-[80px] lg:fixed lg:h-screen lg:w-[103px] lg:flex-col lg:items-center lg:rounded-r-2xl">
+      <Link
+        className="bg-first relative grid w-[72px] place-content-center overflow-hidden rounded-r-2xl lg:h-[103px] lg:w-full"
+        href={'/'}
+      >
         <div className="bg-second absolute bottom-0 h-1/2 w-[72px] rounded-tl-2xl lg:w-full " />
         <svg
           className="z-10 lg:h-10 lg:w-10"
@@ -24,7 +28,7 @@ export async function Header() {
             d="M20.513 0C24.965 2.309 28 6.91 28 12.21 28 19.826 21.732 26 14 26S0 19.826 0 12.21C0 6.91 3.035 2.309 7.487 0L14 12.9z"
           />
         </svg>
-      </div>
+      </Link>
       <div className="flex items-center gap-6 md:gap-8 lg:flex-col">
         <ModeToggleButton />
         <div className="grid h-full w-[80px] place-content-center border-l border-[#494E6E] md:w-[96px] lg:h-[88px] lg:w-[103px] lg:border-l-0 lg:border-t">
