8b5bbb5211ba01d0329f74d8119aaee65a9b1ec2
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-25 13:03:13 +0200

    feat: Add InvoiceDetail type for representing detailed invoice data

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -25,3 +25,35 @@ export type Invoice = {
   client_address_id: string;
   user_id: string;
 };
+
+export type InvoiceDetail = {
+  id: string;
+  created_at: Date;
+  payment_due: Date;
+  description: string;
+  payment_terms: number;
+  client_name: string;
+  client_email: string;
+  status: 'paid' | 'pending' | 'draft';
+  total: number;
+  sender_address_id: string;
+  client_address_id: string;
+  user_id: string;
+  sender_street: string;
+  sender_city: string;
+  sender_postcode: string;
+  sender_country: string;
+  client_street: string;
+  client_city: string;
+  client_postcode: string;
+  client_country: string;
+  items: {
+    id: string;
+    name: string;
+    quantity: number;
+    price: number;
+    total: number;
+    invoice_id: string;
+  }[];
+  grand_total: number;
+};
