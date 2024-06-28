102f5b308796ca53e21300585764d7dc890e5afe
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-28 09:47:28 +0200

    chore: Update toast success variant color to green

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         default: 'border bg-background text-foreground',
         destructive:
           'destructive group border-destructive bg-destructive text-destructive-foreground',
-        success: 'border-success bg-success text-success-foreground',
+        success: 'border-success bg-green-500 text-success-foreground',
       },
     },
     defaultVariants: {
