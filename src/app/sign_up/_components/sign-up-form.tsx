4e1fa86497e5f8957dc5cedc4f2768e4a5652f0d
Denis Pianelli <denis.pianelli@gmail.com>
2024-07-02 14:36:31 +0200

    chore: Remove unused input components and update sign-up form layout and styles

--- src/app/sign_up/_components/sign-up-form.tsx
+++ src/app/sign_up/_components/sign-up-form.tsx
@@ -30,18 +30,6 @@ import { useForm, useFormState } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const SignupFormSchema = z.object({
-  firstname: z
-    .string()
-    .min(2, { message: 'At least 2 characters.' })
-    .trim()
-    .optional()
-    .or(z.literal('')),
-  lastname: z
-    .string()
-    .min(2, { message: 'Name must be at least 2 characters long.' })
-    .trim()
-    .optional()
-    .or(z.literal('')),
   email: z
     .string()
     .min(1, { message: 'Email is required.' })
@@ -74,8 +62,6 @@ export function SignUpForm() {
   const form = useForm<z.infer<typeof SignupFormSchema>>({
     resolver: zodResolver(SignupFormSchema),
     defaultValues: {
-      firstname: '',
-      lastname: '',
       email: '',
       password: '',
     },
@@ -101,7 +87,7 @@ export function SignUpForm() {
   }
 
   return (
-    <Card className="mx-auto max-w-sm">
+    <Card className="mx-auto w-[320px]">
       <CardHeader>
         <CardTitle className="text-xl">Sign Up</CardTitle>
         <CardDescription>
@@ -112,51 +98,13 @@ export function SignUpForm() {
         <div className="grid gap-4">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-2">
-              <div className="grid grid-cols-2 gap-4">
-                <FormField
-                  control={form.control}
-                  name="firstname"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>
-                        First name{' '}
-                        <span className="text-xs text-gray-400">
-                          (optional)
-                        </span>
-                      </FormLabel>
-                      <FormControl>
-                        <FormInput placeholder="Max" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="lastname"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>
-                        Last name{' '}
-                        <span className="text-xs text-gray-400">
-                          (optional)
-                        </span>
-                      </FormLabel>
-                      <FormControl>
-                        <FormInput placeholder="Robinson" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
               <FormField
                 control={form.control}
                 name="email"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>
-                      Email <span className="text-red-500">*</span>
+                      Email <span className="text-destructive">*</span>
                     </FormLabel>
                     <FormControl>
                       <FormInput
@@ -179,7 +127,7 @@ export function SignUpForm() {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>
-                      Password <span className="text-red-500">*</span>
+                      Password <span className="text-destructive">*</span>
                     </FormLabel>
                     <FormControl>
                       <FormInput
@@ -196,10 +144,6 @@ export function SignUpForm() {
               <SignUpButton isEmailFree={isEmailFree} />
             </form>
           </Form>
-          <div className="mx-auto my-4 flex w-full items-center justify-evenly text-sm font-semibold text-gray-600 before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
-            or continue with
-          </div>
-          <OAuthProviders />
         </div>
         <div className="mt-4 text-center text-sm">
           Already have an account?{' '}
@@ -218,7 +162,7 @@ function SignUpButton({ isEmailFree }: { isEmailFree: boolean }) {
   return (
     <Button
       type="submit"
-      className="w-full"
+      className="mt-2 w-full"
       aria-disabled={isSubmitting || !isEmailFree}
     >
       {isSubmitting ? (
