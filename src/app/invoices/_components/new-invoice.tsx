df5d1b26add84e43280d2e02deb9c0b4f0a0f361
Denis Pianelli <denis.pianelli@gmail.com>
2024-07-02 14:36:01 +0200

    feat: Add new invoice creation functionality and UI components

--- src/app/invoices/_components/new-invoice.tsx
+++ src/app/invoices/_components/new-invoice.tsx
@@ -1,22 +1,28 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import styles from '@/components/ui/home.module.css';
 import {
   Sheet,
   SheetClose,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
 import ArrowLeft from '../[id]/_components/arrow-left';
-import { InvoiceForm } from '../new/_components/new-invoice-form';
+import { InvoiceForm } from './new-invoice-form';
 
 export default function NewInvoice() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button className="relative w-[90px] p-0" variant={'one'}>
-          <div className="absolute left-[6px] flex items-center gap-2">
+        <Button
+          className="relative h-[44px] w-[90px] p-0 md:h-[48px] md:w-[150px]"
+          variant={'one'}
+        >
+          <div className="absolute left-[6px] flex items-center gap-2 md:left-[8px] md:gap-[16px]">
             <div className="flex h-8 w-8 items-center justify-center rounded-full bg-white">
               <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -26,12 +32,14 @@ export default function NewInvoice() {
                 />
               </svg>
             </div>
-            New
+            <p>
+              New <span className="hidden md:inline">Invoice</span>
+            </p>
           </div>
         </Button>
       </SheetTrigger>
       <SheetContent
-        className={`mt-[70px] max-h-[calc(100vh-72px)] w-screen overflow-y-auto bg-white p-0 pb-0 pt-9 dark:bg-[#141625] md:mt-[80px] md:max-h-[calc(100vh-80px)] md:w-[616px] md:overflow-y-hidden md:rounded-r-[20px] ${styles['scroll-container']}`}
+        className={`mt-[70px] max-h-[calc(100vh-72px)] w-screen overflow-y-auto overflow-x-hidden bg-white p-0 pb-0 pt-9 dark:bg-[#141625] md:mt-[80px] md:max-h-[calc(100vh-80px)] md:w-[616px] md:overflow-y-hidden md:rounded-r-[20px] lg:mt-0 lg:max-h-screen lg:translate-x-[103px] ${styles['scroll-container']}`}
         side={'left'}
       >
         <SheetHeader>
@@ -45,6 +53,7 @@ export default function NewInvoice() {
             </Button>
           </SheetClose>
           <SheetTitle className="text-left">New Invoice</SheetTitle>
+          <SheetDescription />
         </SheetHeader>
         <InvoiceForm />
       </SheetContent>
