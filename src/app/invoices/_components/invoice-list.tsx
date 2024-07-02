54e44555fce14cf0d723c2e11de27ced97069092
Denis Pianelli <denis.pianelli@gmail.com>
2024-07-02 14:35:53 +0200

    chore: Update InvoiceList component to include pagination and handle status filters

--- src/app/invoices/_components/invoice-list.tsx
+++ src/app/invoices/_components/invoice-list.tsx
@@ -4,27 +4,39 @@ import { User } from '@/lib/definitions';
 import { fetchFilteredInvoices } from '@/lib/data';
 import InvoiceListHeader from './invoice-list-header';
 import NoInvoices from './no-invoices';
+import { getUser } from '@/lib/dal';
+import { redirect } from 'next/navigation';
 
 export default async function InvoiceList({
-  user,
   status,
+  currentPage,
+  count,
 }: {
-  user: User;
   status?: string;
+  currentPage: number;
+  count: number;
 }) {
+  const user: User | null = await getUser();
+
+  if (!user) {
+    redirect('/login');
+  }
+
   const filters = status?.split('.');
 
-  const invoices = await fetchFilteredInvoices(user.id, filters);
+  const invoices = await fetchFilteredInvoices(user.id, currentPage, filters);
 
   return (
     <>
-      <InvoiceListHeader filters={filters} filteredInvoices={invoices} />
-      {invoices.map((invoice) => (
-        <Link key={invoice.id} href={`/invoices/${invoice.id}`}>
-          <InvoiceInfos invoice={invoice} />
-        </Link>
-      ))}
-      {!invoices.length && <NoInvoices />}
+      <InvoiceListHeader filters={filters} count={count} />
+      <div className="h-[910px] md:h-[640px]">
+        {invoices.map((invoice) => (
+          <Link key={invoice.id} href={`/invoices/${invoice.id}`}>
+            <InvoiceInfos invoice={invoice} />
+          </Link>
+        ))}
+        {!invoices.length && <NoInvoices />}
+      </div>
     </>
   );
 }
