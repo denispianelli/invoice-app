96d6ae3101ddc90d0a9cc056a92a5dc8684cfa04
Denis Pianelli <denis.pianelli@gmail.com>
2024-07-02 14:37:15 +0200

    chore: Update placeholder data with correct decimal formatting for prices and totals

--- src/lib/placeholder-data.js
+++ src/lib/placeholder-data.js
@@ -8,9 +8,8 @@ const users = [
   },
 ];
 
-
 const addresses = [
-	{
+  {
     id: '43ee452d-fc66-4dfb-a963-eed6b3b0c211',
     type: 'sender',
     street: '19 Union Terrace',
@@ -78,10 +77,10 @@ const invoices = [
     clientName: 'Jensen Huang',
     clientEmail: 'jensenh@mail.com',
     status: 'paid',
-		total: 1800.90,
+    total: 1800.9,
     sender_address_id: addresses[0].id,
     client_address_id: addresses[1].id,
-		user_id: users[0].id,
+    user_id: users[0].id,
   },
   {
     id: 'XM9141',
@@ -92,10 +91,10 @@ const invoices = [
     clientName: 'Alex Grim',
     clientEmail: 'alexgrim@mail.com',
     status: 'pending',
-		total: 556.00,
+    total: 556.0,
     sender_address_id: addresses[0].id,
     client_address_id: addresses[2].id,
-		user_id: users[0].id,
+    user_id: users[0].id,
   },
   {
     id: 'RG0314',
@@ -106,10 +105,10 @@ const invoices = [
     clientName: 'John Morrison',
     clientEmail: 'jm@myco.com',
     status: 'paid',
-		total: 14002.33,
+    total: 14002.33,
     sender_address_id: addresses[0].id,
     client_address_id: addresses[3].id,
-		user_id: users[0].id,
+    user_id: users[0].id,
   },
   {
     id: 'RT2080',
@@ -120,10 +119,10 @@ const invoices = [
     clientName: 'Alysa Werner',
     clientEmail: 'alysa@email.co.uk',
     status: 'pending',
-		total: 102.04,
+    total: 102.04,
     sender_address_id: addresses[0].id,
     client_address_id: addresses[4].id,
-		user_id: users[0].id,
+    user_id: users[0].id,
   },
   {
     id: 'AA1449',
@@ -134,10 +133,10 @@ const invoices = [
     clientName: 'Mellisa Clarke',
     clientEmail: 'mellisa.clarke@example.com',
     status: 'pending',
-		total: 4032.33,
+    total: 4032.33,
     sender_address_id: addresses[0].id,
     client_address_id: addresses[5].id,
-		user_id: users[0].id,
+    user_id: users[0].id,
   },
   {
     id: 'TY9141',
@@ -148,13 +147,13 @@ const invoices = [
     clientName: 'Thomas Wayne',
     clientEmail: 'thomas@dc.com',
     status: 'pending',
-		total: 6155.91,
+    total: 6155.91,
     sender_address_id: addresses[0].id,
     client_address_id: addresses[6].id,
-		user_id: users[0].id,
+    user_id: users[0].id,
   },
-	{
-		id: 'FV2353',
+  {
+    id: 'FV2353',
     createdAt: '2021-11-05',
     paymentDue: '2021-11-12',
     description: 'Logo Re-design',
@@ -162,20 +161,20 @@ const invoices = [
     clientName: 'Anita Wainwright',
     clientEmail: '',
     status: 'draft',
-		total: 3102.04,
+    total: 3102.04,
     sender_address_id: addresses[0].id,
-		user_id: users[0].id,
-	}
+    user_id: users[0].id,
+  },
 ];
 
 const items = [
-	{
+  {
     id: '233d7bae-71a0-49fd-9dfb-aa874ab6429b',
     name: 'Brand Guidelines',
     quantity: 1,
-    price: 1800.90,
-    total: 1800.90,
-		invoice_id: invoices[0].id,
+    price: 1800.9,
+    total: 1800.9,
+    invoice_id: invoices[0].id,
   },
   {
     id: '2049d9fc-5310-448c-aba4-7ef7cb14ed1c',
@@ -183,7 +182,7 @@ const items = [
     quantity: 1,
     price: 156.0,
     total: 156.0,
-		invoice_id: invoices[1].id,
+    invoice_id: invoices[1].id,
   },
   {
     id: '69b7d511-03e0-4304-abfa-032df3dca07c',
@@ -191,7 +190,7 @@ const items = [
     quantity: 2,
     price: 200.0,
     total: 400.0,
-		invoice_id: invoices[1].id,
+    invoice_id: invoices[1].id,
   },
   {
     id: '8267964b-0c18-4513-84aa-f4444435bbff',
@@ -199,7 +198,7 @@ const items = [
     quantity: 1,
     price: 14002.33,
     total: 14002.33,
-		invoice_id: invoices[2].id,
+    invoice_id: invoices[2].id,
   },
   {
     id: '47749729-fc3c-4d69-994f-7897263d4e9a',
@@ -207,7 +206,7 @@ const items = [
     quantity: 1,
     price: 102.04,
     total: 102.04,
-		invoice_id: invoices[3].id,
+    invoice_id: invoices[3].id,
   },
   {
     id: '7606361a-a6b8-450e-95f9-eef132a2c65d',
@@ -215,7 +214,7 @@ const items = [
     quantity: 1,
     price: 1532.33,
     total: 1532.33,
-		invoice_id: invoices[4].id,
+    invoice_id: invoices[4].id,
   },
   {
     id: '4a9fa63d-6e7e-43b2-adb9-cd62783b292b',
@@ -223,7 +222,7 @@ const items = [
     quantity: 1,
     price: 2500.0,
     total: 2500.0,
-		invoice_id: invoices[4].id,
+    invoice_id: invoices[4].id,
   },
   {
     id: '62766ee9-f606-49fc-a513-61c0844a73e9',
@@ -231,7 +230,7 @@ const items = [
     quantity: 1,
     price: 6155.91,
     total: 6155.91,
-		invoice_id: invoices[5].id,
+    invoice_id: invoices[5].id,
   },
   {
     id: '0f50592d-601c-4061-a722-3550a6d3b5da',
@@ -239,13 +238,13 @@ const items = [
     quantity: 1,
     price: 3102.04,
     total: 3102.04,
-		invoice_id: invoices[6].id,
+    invoice_id: invoices[6].id,
   },
 ];
 
 module.exports = {
-	users,
-	addresses,
-	invoices,
-	items,
+  users,
+  addresses,
+  invoices,
+  items,
 };
